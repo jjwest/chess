1a31a762fa1bd37bcc9d31b27a3837d59218789f
Jonas Vestlund <jonve547@student.liu.se>
2016-11-14 15:10:22 +0100

    Breaking out check functions

--- Chess/Chess/GameLogic.cs
+++ Chess/Chess/GameLogic.cs
@@ -8,62 +8,69 @@ using Database;
 
 namespace Chess
 {
-	public class GameLogic
+    public class GameLogic
+    {
+        private DatabaseInterface database;
+        private RuleBook ruleBook;
+
+        public GameLogic(DatabaseInterface _database, RuleBook _rules)
+        {
+            database = _database;
+            ruleBook = _rules;
+        }
+
+        public GameStateEntity MovePiece(GameMoveEntity piece)
+        {
+            var gameState = database.GetState ();
+
+            if (ruleBook.MoveIsValid(piece, gameState))
+            {
+        	ExecuteMove (piece, gameState);
+		database.SaveState (gameState);
+		return database.GetState ();
+	    }
+
+	    return gameState;
+	}
+
+	private void ExecuteMove(GameMoveEntity piece, GameStateEntity state)
 	{
-		private DatabaseInterface database;
-		private RuleBook ruleBook;
-
-		public GameLogic(DatabaseInterface _database, RuleBook _rules)
-		{
-			database = _database;
-			ruleBook = _rules;
-		}
-
-		public GameStateEntity MovePiece(GameMoveEntity piece)
-		{
-			var gameState = database.GetState ();
-
-			if (ruleBook.MoveIsValid(piece, gameState)) 
-			{
-				ExecuteMove (piece, gameState);
-				database.SaveState (gameState);
-				return database.GetState ();
-			}
-
-			return gameState;
-		}
-
-		private void ExecuteMove(GameMoveEntity piece, GameStateEntity state)
-		{
             bool hasMoved = true;
             var movedPiece = state.GameBoard[piece.CurrentPos.Y][piece.CurrentPos.X];
             var target = state.GameBoard[piece.RequestedPos.Y] [piece.RequestedPos.X];
 
             if (Castling(movedPiece, target))
-                PerformCastling(piece, state);
+            {
+                state = PerformCastling(piece, state);
+            }
+//            else if (KingIsChecked(piece, state))
+//            {
+//                state.KingIsChecked = true;
+//            }
             else
             {
                 state.GameBoard[piece.CurrentPos.Y][piece.CurrentPos.X] = new GamePiece(PieceType.None, Color.None);
                 state.GameBoard[piece.RequestedPos.Y] [piece.RequestedPos.X] = new GamePiece(piece.Type, piece.Color, hasMoved);
             }
-          
-			state.ActivePlayer = state.ActivePlayer == Color.White ? Color.Black : Color.White;
-		}
+
+	    state.ActivePlayer = state.ActivePlayer == Color.White ? Color.Black : Color.White;
+
+	}
 
         private bool Castling(GamePiece movedPiece, GamePiece target)
         {
             return movedPiece.Type == PieceType.King &&
-                   target.Type == PieceType.Rook &&
-                   movedPiece.Color == target.Color;
+		           target.Type == PieceType.Rook &&
+		           movedPiece.Color == target.Color;
         }
 
-        private void PerformCastling(GameMoveEntity piece, GameStateEntity state)
+        private GameStateEntity PerformCastling(GameMoveEntity piece, GameStateEntity state)
         {
             var hasMoved = true;
             if (piece.Color == Color.White)
             {
                 if (piece.RequestedPos.X == 7)
-                {                   
+                {
                     state.GameBoard[7][7] = new GamePiece(PieceType.None, Color.None);
                     state.GameBoard[7][4] = new GamePiece(PieceType.None, Color.None);
                     state.GameBoard[7][6] = new GamePiece(PieceType.King, Color.White, hasMoved);
@@ -94,18 +101,19 @@ namespace Chess
                     state.GameBoard[0][2] = new GamePiece(PieceType.Rook, Color.Black, hasMoved);
                 }
             }
+
+            return state;
         }
 
-		public GameStateEntity TransformPiece(GameMoveEntity piece)
-		{
-			var gameState = database.GetState ();
+	public GameStateEntity TransformPiece(GameMoveEntity piece)
+	{
+	    var gameState = database.GetState ();
             gameState.GameBoard [piece.RequestedPos.Y] [piece.RequestedPos.X] = new GamePiece(piece.Type, piece.Color);
-			database.SaveState(gameState);
+	    database.SaveState(gameState);
 
-			return gameState;
-		}
-
-
+	    return gameState;
 	}
-}
 
+
+    }
+}
--- Chess/Chess/Rules.cs
+++ Chess/Chess/Rules.cs
@@ -42,13 +42,79 @@ namespace Rules
             int currY = piece.CurrentPos.Y;
 
             for (int i = 1; i < Math.Max(Math.Abs(deltaX), Math.Abs(deltaY)); i++)
-    		{
+	    {
                 if (state.GameBoard[currY + i * stepY][currX + i * stepX].Type != PieceType.None)
                     return false;
     	    }
-    		
+
             return true;
     	}
+
+        public static bool KingIsChecked(GameMoveEntity piece, GameStateEntity state)
+        {
+            var board = Movement.DeepCopy(state.GameBoard);
+            board[piece.RequestedPos.Y][piece.RequestedPos.X] = board[piece.CurrentPos.Y][piece.CurrentPos.X];
+            board[piece.CurrentPos.Y][piece.CurrentPos.X] = new GamePiece(PieceType.None, Color.None);
+            Point king = FindKing(piece, state);
+
+            for (int y = 0; y < state.GameBoard.Length; y++)
+            {
+                for (int x = 0; x < state.GameBoard[y].Length; x++)
+                {
+                    var type = state.GameBoard[y][x].Type;
+                    var color = state.GameBoard[y][x].Color;
+                    if ((type == PieceType.Bishop || type == PieceType.Queen) && color != piece.Color)
+                    {
+                        var potentialThreat = new GameMoveEntity(type, new Point(x, y), new Point(king.X, king.Y), piece.Color);
+                        if (Movement.IsDiagonal(potentialThreat, state) && Movement.PathIsClear(potentialThreat, state))
+                            return false;
+                    }
+                    if ((type == PieceType.Rook || type == PieceType.Queen) && color != piece.Color)
+                    {
+                        var potentialThreat = new GameMoveEntity(type, new Point(x, y), new Point(king.X, king.Y), piece.Color);
+                        if (Movement.IsLinear(potentialThreat, state) && Movement.PathIsClear(potentialThreat, state))
+                            return false;
+                    }
+                }
+            }
+
+            Console.WriteLine("NOT CHECKED");
+        //          state.GameBoard[piece.CurrentPos.Y][piece.CurrentPos.X] = liftedPiece;
+            return true;
+        }
+
+        private static GamePiece[][] DeepCopy(GamePiece[][] original)
+        {
+            GamePiece[][] newBoard = new GamePiece[original.Length][];
+            for (int i = 0; i < newBoard.Length; i++)
+            {
+                newBoard[i] = (GamePiece[])original[i].Clone();
+            }
+
+            return newBoard;
+        }
+
+        private static Point FindKing(GameMoveEntity piece, GameStateEntity state)
+        {
+            Point king = new Point(0, 0);
+
+            for (int y = 0; y < state.GameBoard.Length; y++)
+            {
+                for (int x = 0; x < state.GameBoard[y].Length; x++)
+                {
+                    var type = state.GameBoard[y][x].Type;
+                    var color = state.GameBoard[y][x].Color;
+                    if (type == PieceType.King && color == piece.Color)
+                    {
+                        king = new Point(x, y);
+                        break;
+                    }
+                }
+            }
+
+            return king;
+      }
+
     }
 
     public class OnlyMoveOwnPiece : Rule
@@ -59,32 +125,32 @@ namespace Rules
         }
     }
 
-	public class RookMovement : Rule
+    public class RookMovement : Rule
+    {
+	public bool IsValid(GameMoveEntity piece, GameStateEntity state)
 	{
-	    public bool IsValid(GameMoveEntity piece, GameStateEntity state)
-	    {
-            if (!piece.Type.Equals(PieceType.Rook)) 
+            if (!piece.Type.Equals(PieceType.Rook))
                 return true;
-    		if (Movement.StepOnOwnPiece(piece, state))
-                return false; 
-    		
-            return Movement.IsLinear(piece, state) && Movement.PathIsClear(piece, state);
-	    }
-	}
+	    if (Movement.StepOnOwnPiece(piece, state))
+                return false;
 
-	public class PawnMovement : Rule
+            return Movement.IsLinear(piece, state) && Movement.PathIsClear(piece, state);
+	}
+    }
+
+    public class PawnMovement : Rule
+    {
+	public bool IsValid(GameMoveEntity piece, GameStateEntity state)
 	{
-	    public bool IsValid(GameMoveEntity piece, GameStateEntity state)
-	    {
-            if (!piece.Type.Equals(PieceType.Pawn)) 
+            if (!piece.Type.Equals(PieceType.Pawn))
                 return true;
-            if (Movement.StepOnOwnPiece(piece, state)) 
-                return false; 
-      
-            return NormalMovement(piece, state) || 
-                   ChargeMovement(piece, state) || 
-                   AttackMovement(piece, state);
-	    }
+            if (Movement.StepOnOwnPiece(piece, state))
+                return false;
+
+            return NormalMovement(piece, state) ||
+		ChargeMovement(piece, state) ||
+		AttackMovement(piece, state);
+	}
 
         private bool NormalMovement(GameMoveEntity piece, GameStateEntity state)
         {
@@ -123,26 +189,26 @@ namespace Rules
             else
                 return deltaX == 1 && deltaY == 1 && target.Color == Color.Black;
         }
-	}
+    }
 
-	public class BishopMovement : Rule
+    public class BishopMovement : Rule
+    {
+	public bool IsValid(GameMoveEntity piece, GameStateEntity state)
 	{
-	    public bool IsValid(GameMoveEntity piece, GameStateEntity state)
-	    {
-    		if (!piece.Type.Equals(PieceType.Bishop)) 
-                return true;
-    		if (Movement.StepOnOwnPiece(piece, state))
+	    if (!piece.Type.Equals(PieceType.Bishop))
+		return true;
+	    if (Movement.StepOnOwnPiece(piece, state))
                 return false;
 
             return Movement.IsDiagonal(piece, state) && Movement.PathIsClear(piece, state);
-	    }
 	}
+    }
 
-	public class KnightMovement : Rule
+    public class KnightMovement : Rule
+    {
+	public bool IsValid(GameMoveEntity piece, GameStateEntity state)
 	{
-	    public bool IsValid(GameMoveEntity piece, GameStateEntity state)
-	    {
-            if (!piece.Type.Equals(PieceType.Knight)) 
+            if (!piece.Type.Equals(PieceType.Knight))
                 return true;
             if (Movement.StepOnOwnPiece(piece, state))
                 return false;
@@ -151,121 +217,77 @@ namespace Rules
             int deltaY = Math.Abs(piece.RequestedPos.Y - piece.CurrentPos.Y);
 
             return deltaX == 2 && deltaY == 1 || deltaX == 1 && deltaY == 2;
-	    }
 	}
+    }
 
-	public class QueenMovement : Rule
+    public class QueenMovement : Rule
+    {
+	public bool IsValid(GameMoveEntity piece, GameStateEntity state)
 	{
-	    public bool IsValid(GameMoveEntity piece, GameStateEntity state)
-	    {
-            if (!piece.Type.Equals(PieceType.Queen)) 
+            if (!piece.Type.Equals(PieceType.Queen))
                 return true;
-            if (Movement.StepOnOwnPiece(piece, state)) 
+            if (Movement.StepOnOwnPiece(piece, state))
                 return false;
 
             return ((Movement.IsLinear(piece, state) || Movement.IsDiagonal(piece, state)) &&
-                     Movement.PathIsClear(piece, state));
-	    }
+		    Movement.PathIsClear(piece, state));
 	}
+    }
 
 
-	public class KingMovement : Rule
+    public class KingMovement : Rule
+    {
+	public bool IsValid(GameMoveEntity piece, GameStateEntity state)
 	{
-	    public bool IsValid(GameMoveEntity piece, GameStateEntity state)
-	    {
-            if (!piece.Type.Equals(PieceType.King)) 
+            if (!piece.Type.Equals(PieceType.King))
                 return true;
-            
+
             return CastleMovement(piece, state) || NormalMovement(piece, state);
-	    }
+	}
 
         private bool CastleMovement(GameMoveEntity piece, GameStateEntity state)
         {
             var king = state.GameBoard[piece.CurrentPos.Y][piece.CurrentPos.X];
             var rook = state.GameBoard[piece.RequestedPos.Y][piece.RequestedPos.X];
+            Point newKingPos = piece.RequestedPos;
 
-            return !king.HasMoved && 
-                   !rook.HasMoved && 
-                    rook.Type == PieceType.Rook && 
-                    Movement.PathIsClear(piece, state) &&
-                    Movement.StepOnOwnPiece(piece, state);
+            if (piece.RequestedPos.X == 0 && piece.RequestedPos.Y == 0)
+                newKingPos = new Point(1, 0);
+            else if (piece.RequestedPos.X == 7 && piece.RequestedPos.Y == 0)
+                newKingPos = new Point(5, 0);
+            else if (piece.RequestedPos.X == 0 && piece.RequestedPos.Y == 7)
+                newKingPos = new Point(2, 7);
+            else if (piece.RequestedPos.X == 7 && piece.RequestedPos.Y == 7)
+                newKingPos = new Point(6, 7);
+
+            GameMoveEntity mockMove = new GameMoveEntity(PieceType.King, piece.CurrentPos, newKingPos, piece.Color);
+
+
+            return !king.HasMoved &&
+	           	   !rook.HasMoved &&
+		            rook.Type == PieceType.Rook &&
+		            Movement.PathIsClear(piece, state) &&
+		            Movement.StepOnOwnPiece(piece, state) &&
+                   !Movement.KingIsChecked(mockMove, state);
         }
 
         private bool NormalMovement(GameMoveEntity piece, GameStateEntity state)
         {
-            if (Movement.StepOnOwnPiece(piece, state)) 
+            if (Movement.StepOnOwnPiece(piece, state))
                 return false;
-           
+
             int deltaX = Math.Abs(piece.RequestedPos.X - piece.CurrentPos.X);
             int deltaY = Math.Abs(piece.RequestedPos.Y - piece.CurrentPos.Y);
 
             return deltaX < 2 && deltaY < 2;
         }
-	}
+    }
 
-	public class Check : Rule
-	{
-	    public bool IsValid(GameMoveEntity piece, GameStateEntity state)
-	    {
-            GamePiece liftedPiece = state.GameBoard[piece.CurrentPos.Y][piece.CurrentPos.X];
-            state.GameBoard[piece.CurrentPos.Y][piece.CurrentPos.X] = new GamePiece(PieceType.None, Color.None);
-            Point king = FindKing(piece, state);
-                
-            for (int y = 0; y < state.GameBoard.Length; y++)
-            {
-                for (int x = 0; x < state.GameBoard[y].Length; x++)
-                {
-                    var type = state.GameBoard[y][x].Type;
-                    var color = state.GameBoard[y][x].Color;
-                    if ((type == PieceType.Bishop || type == PieceType.Queen) && color != piece.Color)
-                    {
-                        var potentialThreat = new GameMoveEntity(type, new Point(x, y), new Point(king.X, king.Y), piece.Color);
-                        if (Movement.IsDiagonal(potentialThreat, state) && Movement.PathIsClear(potentialThreat, state))
-                        {
-                            state.GameBoard[piece.CurrentPos.Y][piece.CurrentPos.X] = liftedPiece;
-                            return false;
-                        }
-                    }
-                    if ((type == PieceType.Rook || type == PieceType.Queen) && color != piece.Color)
-                    {
-                        var potentialThreat = new GameMoveEntity(type, new Point(x, y), new Point(king.X, king.Y), piece.Color);
-                        if (Movement.IsLinear(potentialThreat, state) && Movement.PathIsClear(potentialThreat, state))
-                        {
-                            state.GameBoard[piece.CurrentPos.Y][piece.CurrentPos.X] = liftedPiece;
-                            return false;
-                        }
-                    }
-                }
-            }
-
-            Console.WriteLine("NOT CHECKED");
-  //          state.GameBoard[piece.CurrentPos.Y][piece.CurrentPos.X] = liftedPiece;
-            return true;
-	    }
-            
-
-        private Point FindKing(GameMoveEntity piece, GameStateEntity state)
-        {
-            if (piece.Type == PieceType.King)
-                return piece.RequestedPos;
-        
-            Point king = new Point(0, 0);
-             
-            for (int y = 0; y < state.GameBoard.Length; y++)
-            {
-                for (int x = 0; x < state.GameBoard[y].Length; x++)
-                {
-                    var type = state.GameBoard[y][x].Type;
-                    var color = state.GameBoard[y][x].Color;
-                    if (type == PieceType.King && color == piece.Color)
-                    {
-                        king = new Point(x, y);
-                        break;
-                    }
-                }
-            }
-            
-            return king;
-        }
-	}
+    public class Check : Rule
+    {
+    	public bool IsValid(GameMoveEntity piece, GameStateEntity state)
+    	{
+            return !Movement.KingIsChecked(piece, state);
+    	}
+    }
 }
