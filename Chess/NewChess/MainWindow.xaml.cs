5a21fbd5de7bd2b110aeaa939fc06fc172262720
unknown <jonve547@PC4-4.helix.ida.liu.se>
2016-11-23 12:28:26 +0100

    Finished new game board

--- Chess/NewChess/MainWindow.xaml.cs
+++ Chess/NewChess/MainWindow.xaml.cs
@@ -13,6 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Logic;
+using Database;
+using Rules;
+
 namespace NewChess
 {
     /// <summary>
@@ -23,6 +27,56 @@ namespace NewChess
         public MainWindow()
         {
             InitializeComponent();
+            DrawGameBoard();
+            RuleBook rules = LoadRules();
+            GameLogic logic = new GameLogic(new Database.Database(), rules);
+
+        }
+
+     
+        private static RuleBook LoadRules()
+        {
+            RuleBook standard = new RuleBook();
+            standard.AddRule(new OnlyMoveOwnPiece());
+            standard.AddRule(new BishopMovement());
+            standard.AddRule(new RookMovement());
+            standard.AddRule(new KnightMovement());
+            standard.AddRule(new KingMovement());
+            standard.AddRule(new QueenMovement());
+            standard.AddRule(new PawnMovement());
+            standard.AddRule(new Check());
+
+            return standard;
+        }
+        private void DrawGameBoard()
+        {
+            var converter = new BrushConverter();
+            var blackBrush = (Brush)converter.ConvertFromString("#FF480000");
+            var whiteBrush = (Brush)converter.ConvertFromString("#FFF9D093");
+
+
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    Rectangle rect = new Rectangle();
+                    rect.Height = 60;
+                    rect.Width = 60;
+
+                    if ((x + y) % 2 == 0)
+                    {
+                        rect.Fill = blackBrush;
+                    }
+                    else
+                    {
+                        rect.Fill = whiteBrush;
+                    }
+
+                    this.GameBoard.Children.Add(rect);
+                    Grid.SetColumn(rect, x);
+                    Grid.SetRow(rect, y);
+                }
+            }
         }
     }
 }
